903b57154dddedc75d69d3a27bc8c1642b34a38b
MrShadowX00 <khujaking@gmail.com>
2022-01-20 19:04:33 +0500

    last changes

--- schemas.py
+++ schemas.py
@@ -10,7 +10,7 @@ class SignUpModel(BaseModel):
     password:str
     is_staff:Optional[bool]
     is_active:Optional[bool]
-
+ 
     class Config:
         orm_mode = True
         schema_extra={
@@ -22,3 +22,40 @@ class SignUpModel(BaseModel):
                 "is_active":True
             }
         }
+
+
+class Settings(BaseModel):
+    authjwt_secret_key:str = '9d5a92e5a7e1c0af8e6dbcceda9f633df99b3cd3e0143108b49c8d752ddb1729'
+
+class LoginModel(BaseModel):
+    username:str
+    password:str
+
+class OrderModel(BaseModel):
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    pizza_size:Optional[str]="LARGE"
+    user_id:Optional[int]
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "quantity":2,
+                "pizza_size":"LARGE"
+            }
+        }
+
+
+class OrderStatusModel(BaseModel):
+    order_status:Optional[str]="PENDING"
+
+    class Config:
+        orm_mode=True
+        schema_extra={
+            "example":{
+                "order_status":"PENDING"
+            }
+        }
+
